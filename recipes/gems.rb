4a95e67952ac57d510888939fcc8de21316cb53a
Marcus Mennemeier <m.mennemeier@global-communication.de>
2014-12-09 09:19:42 +0100

    remove version option for gem installs (due to backwards compatibility)

--- recipes/gems.rb
+++ recipes/gems.rb
@@ -1,7 +1,6 @@
 node['chef-admin']['gems'].each do |g|
-  gem_package g[:name] do
+  gem_package g do
     gem_binary '/opt/chefdk/bin/chef gem'
     options '--no-user-install'
-    version g[:version] unless g[:version].nil?
   end
 end
